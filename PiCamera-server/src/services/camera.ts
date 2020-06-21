8eb25b4dc03488062da178aca7bb72a2b480ed61
alberti-tu <albertalvarezmiron@gmail.com>
2020-06-21 12:13:28 +0200

    PiCamera-server

--- PiCamera-server/src/services/camera.ts
+++ PiCamera-server/src/services/camera.ts
@@ -4,7 +4,6 @@ import moment from 'moment';
 import path from 'path';
 import fs from 'fs';
   
-
 export async function takePhoto(options?: { save?: boolean }): Promise<string> {
     const camera = new StillCamera({ flip: configuration.photo.rotate ? Flip.Both : Flip.None });
     const image = await camera.takeImage().catch(err => err);
@@ -14,15 +13,7 @@ export async function takePhoto(options?: { save?: boolean }): Promise<string> {
     }
 
     if (options && options.save) {
-        let num = 0;
-        const date = moment().format('YYYY-MM-DD');
-        const files = await readDirectory(configuration.photo.directory);
-        files.forEach(item => {
-            if (date === item.split('_')[0]) {
-                num = Math.max(num, parseInt(item.split('_')[1].split('.')[0]));
-            }
-        })
-        fs.writeFileSync(configuration.photo.directory + '/' + date + '_' + (num + 1) + '.jpeg', image);
+        fs.writeFileSync(configuration.photo.directory + '/' + await getName(configuration.photo.directory) + '.jpeg', image);
     }
 
     return 'data:image/jpeg;base64,' + image.toString('base64');
@@ -31,7 +22,7 @@ export async function takePhoto(options?: { save?: boolean }): Promise<string> {
 export async function takeVideo(): Promise<any> {
     const camera = new StreamCamera({ codec: Codec.H264, flip: configuration.video.rotate ? Flip.Both : Flip.None });
      
-    const file = fs.createWriteStream(configuration.video.directory + '/video.h264');
+    const file = fs.createWriteStream(configuration.video.directory + '/' + await getName(configuration.photo.directory) + '.h264');
     const stream = camera.createStream();
     stream.pipe(file);
      
@@ -51,4 +42,19 @@ async function readDirectory(directory: string): Promise<string[]> {
             }
         });
     });
+}
+
+async function getName(directory: string): Promise<string> {
+    let num = 0;
+    
+    const date = moment().format('YYYY-MM-DD');
+    const files = await readDirectory(directory);
+    
+    files.forEach(item => {
+        if (date === item.split('_')[0]) {
+            num = Math.max(num, parseInt(item.split('_')[1].split('.')[0]));
+        }
+    });
+
+    return date + '_' + (num + 1);
 }
