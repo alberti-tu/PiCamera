05e552b2a0cb5afaf523e417f5f149d30b7b8fd4
alberti-tu <albertalvarezmiron@gmail.com>
2021-01-25 18:07:44 +0100

    response types

--- Client/src/services/http.services.ts
+++ Client/src/services/http.services.ts
@@ -6,17 +6,17 @@ import { CameraDTO, Message } from '../models/http.models';
 
 const id = getSerialNumber();
 
-export function setup(): Promise<boolean> {
-    return new Promise<boolean>((resolve, reject) => {
-        axios.get<Message<boolean>>(configuration.host + '/api/camera/' + encrypt(id, configuration.sharedKey))
+export function setup(): Promise<CameraDTO> {
+    return new Promise<CameraDTO>((resolve, reject) => {
+        axios.get<Message<CameraDTO>>(configuration.host + '/api/camera/' + encrypt(id, configuration.sharedKey))
             .then(body => resolve(body.data.result))
             .catch(err => reject(err.response.data.result));
     });
 }
 
-export function register(): Promise<any> {
-    return new Promise<CameraDTO>((resolve, reject) => {
-        axios.post<Message<CameraDTO>>(configuration.host + '/api/camera/' + encrypt(id, configuration.sharedKey))
+export function register(): Promise<boolean> {
+    return new Promise<boolean>((resolve, reject) => {
+        axios.post<Message<boolean>>(configuration.host + '/api/camera/' + encrypt(id, configuration.sharedKey))
             .then(body => resolve(body.data.result))
             .catch(err => reject(err.response.data.result));
     });
