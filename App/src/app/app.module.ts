d5343838270b1e9721e6679606f29d49787bb016
alberti-tu <albertalvarezmiron@gmail.com>
2021-03-02 10:56:25 +0100

    angular theme

--- App/src/app/app.module.ts
+++ App/src/app/app.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
 	declarations: [ AppComponent ],
-	imports: [ BrowserModule, AppRoutingModule ],
+	imports: [
+		AppRoutingModule,
+		BrowserModule,
+		BrowserAnimationsModule
+	],
 	providers: [],
 	bootstrap: [ AppComponent ]
 })
