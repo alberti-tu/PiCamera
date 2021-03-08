8eecd8fdc45e9901b69bd535daf8139edaea68dd
alberti-tu <albertalvarezmiron@gmail.com>
2021-03-08 17:19:40 +0100

    toolbar conponent

--- App/src/app/shared.module.ts
+++ App/src/app/shared.module.ts
@@ -5,20 +5,8 @@ import { TranslateModule } from "@ngx-translate/core";
 import { RouterModule } from "@angular/router";
 
 import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
-import { MatDialogModule } from '@angular/material/dialog';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatIconModule } from '@angular/material/icon';
-import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
-import { MatRadioModule } from '@angular/material/radio';
-import { MatRippleModule } from '@angular/material/core';
-import { MatPaginatorModule } from '@angular/material/paginator';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { MatSelectModule } from '@angular/material/select';
 import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatSliderModule } from '@angular/material/slider';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
@@ -30,20 +18,8 @@ import { MatToolbarModule } from '@angular/material/toolbar';
         TranslateModule,
 
         MatButtonModule,
-        MatCardModule,
-        MatDialogModule,
-        MatFormFieldModule,
-        MatIconModule,
-        MatInputModule,
         MatListModule,
-        MatRadioModule,
-        MatRippleModule,
-        MatPaginatorModule,
-        MatProgressSpinnerModule,
-        MatSelectModule,
         MatSidenavModule,
-        MatSliderModule,
-        MatSnackBarModule,
         MatToolbarModule
     ]
 })
