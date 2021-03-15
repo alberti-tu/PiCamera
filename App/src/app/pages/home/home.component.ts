6d27b8ece9f10e18bfa4ceb96ff1cc732a91e3cd
alberti-tu <albertalvarezmiron@gmail.com>
2021-03-15 13:03:44 +0100

    socket service

--- App/src/app/pages/home/home.component.ts
+++ App/src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SocketService } from 'src/app/services/socket/socket.service';
 
 @Component({
 	selector: 'app-home',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-	constructor() { }
+	constructor(private _socket: SocketService) { }
 
 	public ngOnInit(): void { }
 
