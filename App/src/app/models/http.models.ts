ea9133e9e2adbb10cdfd498d03fb2f11dde9ffae
alberti-tu <albertalvarezmiron@gmail.com>
2021-03-17 17:05:08 +0100

    get image name

--- App/src/app/models/http.models.ts
+++ App/src/app/models/http.models.ts
@@ -12,5 +12,6 @@ export interface CameraSubscription {
 
 export interface FrameStream {
     id: string;
+    name: string;
     data: string;
 }
--- App/src/app/pages/home/home.component.ts
+++ App/src/app/pages/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FrameStream } from 'src/app/models/http.models';
+import { HttpService } from 'src/app/services/http/http.service';
 import { SocketService } from 'src/app/services/socket/socket.service';
 
 @Component({
@@ -9,22 +10,32 @@ import { SocketService } from 'src/app/services/socket/socket.service';
 })
 export class HomeComponent implements OnInit, OnDestroy {
 
-	public pictures: FrameStream[] = [];
+	public imageList: FrameStream[] = [];
 
-	constructor(private _socket: SocketService) { }
+	constructor(private _http: HttpService, private _socket: SocketService) { }
 
 	public ngOnInit(): void {
 		this._socket.connect();
 
-		this._socket.getSubscriptions().subscribe(data => {
-			this.pictures = data.map<FrameStream>(item => ({ id: item, data: 'assets/images/error_404.jpg' }));
+		this._socket.getSubscriptions().subscribe(event => {
+			this.imageList = event.map<FrameStream>(item => ({ id: item, name: null, data: 'assets/images/error_404.jpg' }));
+
+			this._http.getSubscriptions().subscribe(data => {
+				this.imageList.forEach(image => {
+					const subscription = data.result.find(item => item.camera_id == image.id);
+
+					if (subscription != null) {
+						image.name = subscription.name;
+					}
+				});
+			});
 		});
 
-		this._socket.getImage().subscribe(image => {
-			const frame = this.pictures.find(item => item.id == image.id);
+		this._socket.getImage().subscribe(event => {
+			const frame = this.imageList.find(item => item.id == event.id);
 
-			if (frame != null && image.data != null) {
-				frame.data = 'data:image/jpg;base64,' + image.data;
+			if (frame != null && event.data != null) {
+				frame.data = 'data:image/jpg;base64,' + event.data;
 			}
 		});
 	}
