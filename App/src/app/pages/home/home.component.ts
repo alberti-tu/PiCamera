7f7875ead806469bc0901a5026946095b5a8bf93
alberti-tu <albertalvarezmiron@gmail.com>
2021-03-19 11:38:59 +0100

    save file

--- App/src/app/pages/home/home.component.ts
+++ App/src/app/pages/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FrameStream } from 'src/app/models/http.models';
+import { AlertService } from 'src/app/services/alert/alert.service';
 import { HttpService } from 'src/app/services/http/http.service';
 import { SocketService } from 'src/app/services/socket/socket.service';
 
@@ -10,14 +11,15 @@ import { SocketService } from 'src/app/services/socket/socket.service';
 export class HomeComponent implements OnInit, OnDestroy {
 
 	public imageList: FrameStream[] = [];
+	public errorImage: string = 'assets/images/error_404.jpg';
 
-	constructor(private _http: HttpService, private _socket: SocketService) { }
+	constructor(private _alert: AlertService, private _http: HttpService, private _socket: SocketService) { }
 
 	public ngOnInit(): void {
 		this._socket.connect();
 
 		this._socket.getSubscriptions().subscribe(event => {
-			this.imageList = event.map<FrameStream>(item => ({ id: item, name: null, data: 'assets/images/error_404.jpg' }));
+			this.imageList = event.map<FrameStream>(item => ({ id: item, name: null, data: this.errorImage }));
 
 			this._http.getAllSubscriptions().subscribe(data => {
 				this.imageList.forEach(image => {
@@ -35,6 +37,14 @@ export class HomeComponent implements OnInit, OnDestroy {
 
 			if (frame != null && event.data != null) {
 				frame.data = 'data:image/jpg;base64,' + event.data;
+			}
+		});
+	}
+
+	public savePicture(frame: FrameStream): void {
+		this._http.savePicture(frame.id, frame.data).subscribe(data => {
+			if (data.result) {
+				this._alert.showToast('toast.info.success');
 			}
 		});
 	}
