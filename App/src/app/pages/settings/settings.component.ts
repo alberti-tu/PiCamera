24265134a82c5bfbc42a69a6b8ab66116986d492
Albert Álvarez <albertalvarezmiron@gmail.com>
2021-08-24 10:57:46 +0200

    delete user

--- App/src/app/pages/settings/settings.component.ts
+++ App/src/app/pages/settings/settings.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { DialogData } from 'src/app/components/dialog/dialog.component';
 import { AlertService } from 'src/app/services/alert/alert.service';
+import { AuthenticationService } from 'src/app/services/authentication/authentication.service';
 import { HttpService } from 'src/app/services/http/http.service';
-import { SocketService } from 'src/app/services/socket/socket.service';
 
 @Component({
 	templateUrl: './settings.component.html',
@@ -9,7 +10,30 @@ import { SocketService } from 'src/app/services/socket/socket.service';
 })
 export class SettingsComponent implements OnInit {
 
-	constructor(private _alert: AlertService, private _http: HttpService, private _socket: SocketService) { }
+	constructor(private _alert: AlertService, private _auth: AuthenticationService, private _http: HttpService) { }
 
 	public ngOnInit(): void {	}
+
+	public remove(): void {
+		const options: DialogData = {
+			header: 'settings.remove.header',
+			message: 'settings.remove.message',
+			buttons: [
+				{ text: 'settings.remove.button.cancel', value: 'cancel' },
+				{ text: 'settings.remove.button.accept', value: 'ok', isPrimary: true }
+			]
+		};
+		this._alert.showDialog(options).subscribe(result => {
+			if (result == null || result.button == 'cancel') {
+				return;
+			}
+
+			this._http.remove().subscribe(data => {
+				if (data.result) {
+					this._auth.removeToken();
+				}
+			});
+
+		});
+	}
 }
