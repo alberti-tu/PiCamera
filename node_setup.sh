4e57ef994053d5d1d66b009a04a59ca53d70f7f4
Albert Álvarez <albertalvarezmiron@gmail.com>
2022-04-26 15:48:29 +0200

    update install script

--- node_setup.sh
+++ node_setup.sh
@@ -18,32 +18,27 @@ echo ""
 echo "********   NODE SETUP   ********"
 
 echo ""
-echo "Step 1: Updating packages"
-echo ""
-apt update -y
-
-echo ""
-echo "Step 2: Removing current Node version"
+echo "Step 1: Removing current Node version"
 echo ""
 rm -r /usr/lib/node_modules/
 apt remove nodejs -y
 
 echo ""
-echo "Step 3: Downloading repository"
+echo "Step 2: Downloading repository"
 curl -fsSL https://deb.nodesource.com/setup_$VERSION.x | sudo bash -
 
-echo "Step 4: Updating Node"
+echo "Step 3: Updating Node"
 echo ""
 apt install nodejs -y
 
 setcap 'cap_net_bind_service=+ep' /usr/bin/node
 
 echo ""
-echo "Step 5: Updating NPM"
+echo "Step 4: Updating NPM"
 npm i -g npm
 
 echo ""
-echo "Step 6: Cleaning installation"
+echo "Step 5: Cleaning installation"
 echo ""
 apt autoremove -y
 
