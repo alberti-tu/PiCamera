758f72a5fc551c74ce5d8860ac208c9692d5c0c2
Albert Álvarez <albertalvarezmiron@gmail.com>
2022-05-01 11:45:37 +0200

    detail pages

--- App/src/app/pages/cameras/cameras.module.ts
+++ App/src/app/pages/cameras/cameras.module.ts
@@ -3,14 +3,17 @@ import { RouterModule, Routes } from "@angular/router";
 import { SharedModule } from "src/app/shared.module";
 
 import { CamerasComponent } from './cameras.component';
+import { DetailComponent } from './detail/detail.component';
 
 const routes: Routes = [
-	{ path: '', component: CamerasComponent }
+	{ path: '', component: CamerasComponent },
+	{ path: ':id', component: DetailComponent }
 ];
 
 @NgModule({
 	declarations: [
-		CamerasComponent
+		CamerasComponent,
+  		DetailComponent
 	],
 	imports: [
 		SharedModule,
--- App/src/app/pages/photos/photos.module.ts
+++ App/src/app/pages/photos/photos.module.ts
@@ -2,14 +2,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from "@angular/router";
 import { SharedModule } from "src/app/shared.module";
 import { PhotosComponent } from './photos.component';
+import { DetailComponent } from './detail/detail.component';
 
 const routes: Routes = [
-	{ path: '', component: PhotosComponent }
+	{ path: '', component: PhotosComponent },
+	{ path: ':id', component: DetailComponent }
 ];
 
 @NgModule({
 	declarations: [
-		PhotosComponent
+		PhotosComponent,
+  		DetailComponent
 	],
 	imports: [
 		SharedModule,
