5b3203ad9e0405a379a21e73483dc4ebb18e048a
Albert Álvarez <albertalvarezmiron@gmail.com>
2022-05-02 09:48:30 +0200

    form login function

--- App/src/app/pages/login/login.component.ts
+++ App/src/app/pages/login/login.component.ts
@@ -1,14 +1,50 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CustomValidator } from 'src/app/components/utils';
+import { AuthenticationService } from 'src/app/services/authentication/authentication.service';
+import { HttpService } from 'src/app/services/http/http.service';
 
 @Component({
 	selector: 'app-login',
 	templateUrl: './login.component.html',
 	styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
 
-	constructor() { }
+	public form: FormGroup;
+	public showPassword: boolean;
 
-	ngOnInit(): void { }
+	constructor(private _auth: AuthenticationService, private _formBuilder: FormBuilder, private _http: HttpService) {
+		this.form = this._formBuilder.group({
+			username: [ '', [ Validators.required, Validators.minLength(8), CustomValidator.whiteSpace ] ],
+			password: [ '', [ Validators.required, Validators.minLength(8), CustomValidator.whiteSpace ] ]
+		});
+
+		this.showPassword = false;
+	}
+
+	public hasError(name: string, error: string): boolean {
+		const control = this.form.get(name);
+		return control && control.touched && control.errors && control.errors[error];
+	}
+
+	public passwordButton(): void {
+		this.showPassword = !this.showPassword;
+	}
+
+	public sendForm(): void {
+		const username = this.form.value.username;
+		const password = this._auth.hash(this.form.value.password);
+
+		this._http.login(username, password).subscribe(data => {
+			if (data.result) {
+				this._auth.setToken(data.result);
+				this.form.reset();
+			} else {
+				this._auth.removeToken();
+				// this._alert.showToast('toast.error.login');
+			}
+		});
+	}
 
 }
