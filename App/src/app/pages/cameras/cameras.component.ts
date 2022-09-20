7aafaae4f707e76374fa7b23bcb35ee95e3c3764
Albert Álvarez <albertalvarezmiron@gmail.com>
2022-09-21 01:30:30 +0200

    camera name autocomplete

--- App/src/app/pages/cameras/cameras.component.ts
+++ App/src/app/pages/cameras/cameras.component.ts
@@ -76,6 +76,7 @@ export class CamerasComponent implements OnInit {
 					id: 'name',
 					label: 'cameras.edit.description',
 					type: 'text',
+					value: camera?.name,
 					requisites: [ Validators.required, CustomValidator.whiteSpace ]
 				},
 			],
