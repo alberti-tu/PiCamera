4a184c3690cc2da43fd84206e6068c1e038b5af7
Albert Álvarez <albertalvarezmiron@gmail.com>
2022-10-23 01:51:26 +0200

    home squema

--- App/src/app/pages/home/home.component.ts
+++ App/src/app/pages/home/home.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ICameraSubscription, Image } from 'src/app/models/http.models';
+import { AlertService } from 'src/app/services/alert/alert.service';
+import { HttpService } from 'src/app/services/http/http.service';
+import { SocketService } from 'src/app/services/socket/socket.service';
 
 @Component({
 	selector: 'app-home',
@@ -7,8 +11,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-	constructor() {	}
+	public subscriptions?: ICameraSubscription[] = undefined;
+	public pictures?: Image[] = undefined;
 
-	ngOnInit(): void {	}
+	constructor(private alert: AlertService, private http: HttpService, private socket: SocketService) {	}
+
+	public ngOnInit(): void {
+		this.subscriptions = [];
+	}
+
+	public savePicture(frame: Image): void {
+		if (frame.id == undefined || frame.data == undefined) {
+			return;
+		}
+
+		this.http.savePicture(frame.id, frame.data.replace('data:image/jpg;base64,', '')).subscribe(data => {
+			if (data.result) {
+				this.alert.showToast('toast.info.saved', 'info');
+			}
+		});
+	}
 
 }
